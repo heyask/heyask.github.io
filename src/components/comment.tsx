dc8e877a3b5fc3a21a11f6fd4baa7144b2499d4a
SpongeCake <sunrivs@gmail.com>
2022-09-26 21:17:56 +0900

    fix: 댓글 중복 로딩 해결, 테마 변경시 즉시 대응

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,26 +1,58 @@
-import React, { createRef, useEffect } from "react";
-import { useTheme } from "@emotion/react";
+import React, { createRef, useEffect, useRef, useState } from "react";
+import { css, useTheme } from "@emotion/react";
+import { fadeIn } from "../styles/keyframes";
 
 export default function Comment({ location }) {
-  const containerRef = createRef<HTMLDivElement>();
+  const containerRef = useRef<HTMLDivElement>();
   const theme = useTheme();
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    const utterances = document.createElement("script");
+    const utterances: HTMLScriptElement = document.createElement("script");
+
+    utterances.onload = (e) => {
+      let cnt = 0;
+      for (let i = 0; i < containerRef.current.children.length; i++) {
+        if (containerRef.current.children[i].nodeName == "DIV") {
+          cnt++;
+          (containerRef.current.children[i].children[0] as HTMLIFrameElement).onload = ((e) => {
+            setLoaded(true);
+          });
+        }
+      }
+
+      // 테마 변경으로 댓긆창이 여러개 로딩되면 마지막것만 남기고 제거
+      if (cnt > 1) {
+        for (let i = 0; i < containerRef.current.children.length - 1; i++) {
+          if (containerRef.current.children[i].nodeName == "DIV") {
+            containerRef.current.removeChild(containerRef.current.children[i]);
+          }
+        }
+      }
+    };
+
     const attributes = {
       src: "https://utteranc.es/client.js",
       repo: "heyask/heyask.github.io",
-      "issue-term": "title",
+      "issue-term": "pathname",
       label: "comment",
       theme: `github-${theme.key}`,
       crossOrigin: "anonymous",
-      async: "true"
+      async: "false",
+      defer: "true"
     };
     Object.entries(attributes).forEach(([key, value]) => {
       utterances.setAttribute(key, value);
     });
     containerRef.current.appendChild(utterances);
-  }, [location, theme]);
+  }, [location.pathname, theme.key]);
 
-  return <div ref={containerRef} />;
+  return <div ref={containerRef} css={css`
+    min-height: 269px;
+    ${loaded && css`
+    iframe {
+      animation: ${fadeIn} 2s;
+    }
+    `}
+  `} />;
 }
